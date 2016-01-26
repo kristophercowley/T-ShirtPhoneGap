d62b49932f560a735f45d97feaa413cd0684891d
Kristopher Cowley <kristopherleecowley@gmail.com>
2016-01-26 11:03:14 -0700

    Product Constructor

--- www/assets/scripts/jQuery-stuff.js
+++ www/assets/scripts/jQuery-stuff.js
@@ -1,8 +1,29 @@
 var images = jQuery('img');
-var loginButton = jQuery('#loginButton')
+
+var loginButton = jQuery('#loginButton');
+
 loginButton.click(myAlert);
+
 function myAlert(){
 	//debugger;
 	//loginButton.style.backgroundColor = "blue";
 	alert("You clicked login")
 }
+
+function grabItem(){
+	
+}
+
+loginButton.click(grabItem);
+
+//Product Constructor
+function Product(title,price,url,quantity,inStock){
+	this.title = title;
+	this.price = price;
+	this.url = url;
+	this.quantity = quantity;
+	this.inStock = function(){
+		return this.quantity > 0;
+	}
+}
+
